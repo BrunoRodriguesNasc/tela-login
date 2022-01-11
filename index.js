0bd74f90977b1a51ca0e2b158ab9f213bd5ace59
Bruno Rodrigues <bruno.rodrigues@ahgora.com.br>
2022-01-11 09:20:48 -0300

    add styles and layout

---
+++ index.js
@@ -0,0 +1,6 @@
+const toogle = () => {
+    let container = document.querySelector('.container');
+    let section = document.querySelector('section');
+    container.classList.toggle('active');
+    section.classList.toggle('active');
+}
